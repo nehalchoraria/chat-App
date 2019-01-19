f895f3a3bbe59cd9d9261f34ac335a896f665068
nehalchoraria <nehalchoraria09@gmail.com>
2019-01-19 23:27:37 +0530

    update

--- login/consumers.py
+++ login/consumers.py
@@ -6,7 +6,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = ''
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,18 +14,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -35,11 +31,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
--- myapp/routing.py
+++ myapp/routing.py
@@ -3,7 +3,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import login.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
             login.routing.websocket_urlpatterns
